c1cdfc4786efcf875ec303ac3c88fbf1169b43ea
Jturtles <93846874+Jturtles@users.noreply.github.com>
2021-11-07 03:33:09 -0500

    Add files via upload

--- DigWallet/digGen.js
+++ DigWallet/digGen.js
@@ -5,10 +5,11 @@ let qrbtn = document.getElementById("uploadbtn");
 qrbtn.addEventListener("click", generateQR);
 
 function generateQR(){
-    let size = "1000x1000";
-    let data = qrtext.value;
-    let baseURL = "http://api.qrserver.com/v1/create-qr-code/";
-
-    let url = `${baseURL}?data=${data}&size=${size}`;
-    qrcode.src = url;
+    if(qrtext.value.length != 0){
+        let size = "1000x1000";
+        let data = qrtext.value;
+        let baseURL = "http://api.qrserver.com/v1/create-qr-code/";
+        let url = `${baseURL}?data=${data}&size=${size}`;
+        qrcode.src = url;
+    }
 }
