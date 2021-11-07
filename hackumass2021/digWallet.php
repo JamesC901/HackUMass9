1272166a837f2292acb1d01977db1d09157b1289
echongmei <60830442+echongmei@users.noreply.github.com>
2021-11-07 05:21:59 -0500

    Update digWallet.php

--- hackumass2021/digWallet.php
+++ hackumass2021/digWallet.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Digital Business card</title>
-        <link rel="stylesheet" type = "text/css" href = "bootstrap-5.1.3-dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="style.css">
+        <style>
+        header {
+            float: left;
+        }
+        body {
+            background: #c1d1d6;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+    </style>
     </head>
     <header>
-        <a class="btn btn-primary" href="login.html" role="button">Log out</a>
-        <a class="btn btn-primary" style="margin: 25px;" href="profile.html" role="button">Profile</a>
+        <a class="btn btn-primary" href=" http://localhost/hackumass2021/DigWallet/login.html" role="button">Log out</a>
+        <a class="btn btn-primary" style="margin: 25px;" href=" http://localhost/hackumass2021/DigWallet/profile.html" role="button">Profile</a>
         <a class="btn btn-primary" href="view.php" role="button">Stored</a>
 
     </header>
@@ -24,14 +31,7 @@
             <input type="file" name="my_image">
             <input type="submit" name="submit" value="Upload">
         </form>
-<!--             <button id="uploadbtn" class="btn btn-success">Generate QR Code</button>
-        </div>
-        <div class="wrapper fadeInDown">
-            <img id="QRcode" class="fadeIn first"/>
-        </div>
-    <script src="digGen.js"></script> -->
     </body>
 </html>
-
 
 
